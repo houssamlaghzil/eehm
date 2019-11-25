989a218c5211c53e26252126025e903cb31f0a06
houssam <hlaghzil@gmail.com>
2019-11-25 10:44:42 +0100

    clean project

--- src/eehm.java
+++ src/eehm.java
@@ -1,14 +1,9 @@
-import javafx.application.Application;
-import javafx.stage.Stage;
 
-public class eehm extends Application {
+
+public class eehm  {
 
     public static void main(String[] args) {
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage primaryStage) {
 
     }
+
 }
